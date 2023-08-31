290ae4dd7f431cc1541b580a4562ed51b06915f3
Abdul Hanan <abha@itu.dk>
2023-08-31 11:26:35 +0200

    Refactor main to print CLI arguments.
    Longer description...
    
    Co-authored-by: Helge <ropf@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
-﻿using Microsoft.VisualBasic.FileIO;
-
-using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Hanan\Documents\Chirp.CLI\chirp_cli_db.csv"))
-{
-    
-    parser.TextFieldType = FieldType.Delimited;
-    parser.SetDelimiters(",");
-    while (!parser.EndOfData) 
-    {
-        //Processing row
-        string[] fields = parser.ReadFields();
-        foreach (string field in fields) 
-        {
-            Console.WriteLine(field);
-        }
-    }
-}
+﻿foreach (var arg in args)
+    Console.WriteLine(arg);
