1ff618cbaee5d7ebbac1dbcb93aef70eb64a6386
Abdul Hanan <abha@itu.dk>
2023-08-31 11:25:31 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Hanan\Documents\Chirp.CLI\chirp_cli_db.csv"))
+{
+    
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
+    while (!parser.EndOfData) 
+    {
+        //Processing row
+        string[] fields = parser.ReadFields();
+        foreach (string field in fields) 
+        {
+            Console.WriteLine(field);
+        }
+    }
+}
