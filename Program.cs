34df08c4831efc25dc55aee2d38732a320cfcf3d
Tan Dang <tanda@itu.dk>
2023-08-31 15:24:00 +0200

    Changed Program class. Co-Authored-by : Helge <ropf@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
-﻿if (args[0] == "read") {
+﻿using System;
+using System.IO;
+
+
+if (args[0] == "read") {
+
+    try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("chirp_cli_db.csv"))
+            {
+                // Read the stream as a string, and write the string to the console.
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
 
 } else if (args[0] == "cheep") {
-
+    Console.WriteLine("No");
 }
