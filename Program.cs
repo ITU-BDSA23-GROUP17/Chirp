512d41e3fd2536b95b14f2400821128d0f25f5cc
Abdul Hanan <hanani.nas@gmail.com>
2023-09-05 10:43:55 +0200

    Init project setup for Chirp

--- Program.cs
+++ Program.cs
@@ -1,24 +1,57 @@
-﻿using System;
-using System.IO;
+﻿using System.Text.RegularExpressions;
 
+var path = "chirp_cli_db.csv";
 
-if (args[0] == "read") {
-
+if(args[0] == "read"){
     try
+    {
+        // Open the text file using a stream reader.
+        using (var sr = new StreamReader(path))
         {
-            // Open the text file using a stream reader.
-            using (var sr = new StreamReader("chirp_cli_db.csv"))
+        Regex regex = new Regex("(?<abc>[A-Za-z]+),\"(?<message>[^\"]*)\",(?<date>[0-9]+)", RegexOptions.IgnoreCase);
+        string data = sr.ReadLine();
+        
+
+        while ((data = sr.ReadLine()) != null)
+        {
+            MatchCollection matches = regex.Matches(data);
+            foreach (Match match in matches)
             {
-                // Read the stream as a string, and write the string to the console.
-                Console.WriteLine(sr.ReadToEnd());
+                DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(match.Groups[3].ToString())).UtcDateTime;
+                Console.WriteLine($"{match.Groups[1]} @ {dateTime}: {match.Groups[2]} ");
             }
         }
-        catch (IOException e)
-        {
-            Console.WriteLine("The file could not be read:");
-            Console.WriteLine(e.Message);
         }
-
-} else if (args[0] == "cheep") {
-    Console.WriteLine("No");
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+} else if (args[0] == "cheep"){
+    using (StreamWriter sw = File.AppendText(path))
+    {
+        if(args.Length == 1){
+            Console.WriteLine("what do you want to chirp? because there is nothing :)");
+        } else {
+            string message = "";
+            var user = Environment.UserName;
+            DateTime dateTime = DateTime.Now;
+            DateTimeOffset dto = new DateTimeOffset(dateTime.ToUniversalTime());
+            long unixDateTime = dto.ToUnixTimeSeconds();
+            foreach (var arg in args)
+            {
+                if(arg != args[0]){
+                    message +=  arg == args[1] ? arg : $" {arg}";
+                }
+            }
+            sw.WriteLine($"{user},{$"\"{message}\""},{unixDateTime}");
+            Console.Write($"{user} @ {dateTime}: {message}");
+        }
+    }	
 }
+ 
+
+
+
+
