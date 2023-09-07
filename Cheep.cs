3af4d045df48216f163bfe2cdc5b23f44c51e109
Abdul Hanan <hanani.nas@gmail.com>
2023-09-07 15:32:38 +0200

    CsvHelper added
    Co-authored-by: Burak <burak-9@live.dk>
    Co-authored-by: Hanan <hanan.nas@gmail.com>
    Co-authored-by: Joshua <jjmcalba@gmail.com>
    Co-authored-by: Julius <juliusdbertelsen@gmail.com>
    Co-authored-by: Tan <tandang2002@live.dk>

---
+++ Cheep.cs
@@ -0,0 +1,9 @@
+namespace Namespace;
+internal class Cheep
+{
+
+  public string Author { get; set; }
+  public string Message { get; set; }
+ public int Timestamp { get; set; }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using CsvHelper;
+using Namespace;
+
 var path = "chirp_cli_db.csv";
-
-
 
 // main function
 if (!File.Exists(path))
@@ -27,49 +29,65 @@ else
 }
 
 // functions to read file
-void readFile(){
-        // Open the text file using a stream reader.
-        using (var sr = new StreamReader(path))
+void readFile()
+{
+   /*  // Open the text file using a stream reader.
+    using (var sr = new StreamReader(path))
+    {
+        Regex regex = new Regex("(?<abc>[A-Za-z]+),\"(?<message>[^\"]*)\",(?<date>[0-9]+)", RegexOptions.IgnoreCase);
+        string data = sr.ReadLine();
+
+
+        while ((data = sr.ReadLine()) != null)
         {
-            Regex regex = new Regex("(?<abc>[A-Za-z]+),\"(?<message>[^\"]*)\",(?<date>[0-9]+)", RegexOptions.IgnoreCase);
-            string data = sr.ReadLine();
-
-
-            while ((data = sr.ReadLine()) != null)
+            MatchCollection matches = regex.Matches(data);
+            foreach (Match match in matches)
             {
-                MatchCollection matches = regex.Matches(data);
-                foreach (Match match in matches)
-                {
-                    DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(match.Groups[3].ToString())).UtcDateTime;
-                    Console.WriteLine($"{match.Groups[1]} @ {dateTime}: {match.Groups[2]} ");
-                }
+                DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(match.Groups[3].ToString())).UtcDateTime;
+                Console.WriteLine($"{match.Groups[1]} @ {dateTime}: {match.Groups[2]} ");
             }
         }
+    } */
+     // modified from source: https://joshclose.github.io/CsvHelper/getting-started/
+    using (var reader = new StreamReader(path))
+    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+        
+        while (csv.Read())
+        {
+            var record = csv.GetRecord<Cheep>();
+            // test to see if it works
+            Console.WriteLine(record.Message);
+        }
+
+    }
+
 }
 
 // function to append file
-void appendFile(string[] args){
-       using (StreamWriter sw = File.AppendText(path))
+void appendFile(string[] args)
+{
+    using (StreamWriter sw = File.AppendText(path))
+    {
+        if (args.Length == 1)
         {
-            if (args.Length == 1)
-            {
-                Console.WriteLine("what do you want to chirp? because there is nothing :)");
-            }
-            else
-            {
-                string message = "";
-                var user = Environment.UserName;
-                DateTime dateTime = DateTime.Now;
-                DateTimeOffset dto = new DateTimeOffset(dateTime.ToUniversalTime());
-                long unixDateTime = dto.ToUnixTimeSeconds();
-                for (int i = 1; i < args.Length; i++)
-                {
-                    message += i == 1 ? args[i] : $" {args[i]}";
-                }
-                sw.WriteLine($"{user},{$"\"{message}\""},{unixDateTime}");
-                Console.Write($"{user} @ {dateTime}: {message}");
-            }
+            Console.WriteLine("what do you want to chirp? because there is nothing :)");
         }
+        else
+        {
+            string message = "";
+            var user = Environment.UserName;
+            DateTime dateTime = DateTime.Now;
+            DateTimeOffset dto = new DateTimeOffset(dateTime.ToUniversalTime());
+            long unixDateTime = dto.ToUnixTimeSeconds();
+            for (int i = 1; i < args.Length; i++)
+            {
+                message += i == 1 ? args[i] : $" {args[i]}";
+            }
+            sw.WriteLine($"{user},{$"\"{message}\""},{unixDateTime}");
+            Console.Write($"{user} @ {dateTime}: {message}");
+        }
+    }
 }
 
 
