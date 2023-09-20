f1ad0502602ada52ffa96a4fcfd35c56e0b72b0c
Abdul Hanan <hanani.nas@gmail.com>
2023-09-20 16:46:55 +0200

    Small fixes and added another test

--- test/DBTest_integration/IntegrationTest.cs
+++ test/DBTest_integration/IntegrationTest.cs
@@ -27,17 +27,24 @@ public class UnitTest1
             _csvDatabase = CSVDatabase<Cheep>.Instance;
         }
 
+        #region Store_TestCode
+        [Fact]
+        public void check_that_database_contains_something()
+        {
+            Assert.NotEmpty(_csvDatabase.Read());
+        }
+        #endregion
 
 
         #region Store_TestCode
         [Theory]
         [ClassData(typeof(CheepRecordData))]
-        public void chek_that_database_contains_something(Cheep cheep)
+        public void check_that_database_has_added_the_new_cheep(Cheep cheep)
         {
             var input = cheep;
             _csvDatabase.Store(input);
 
-            Assert.NotEmpty(_csvDatabase.Read());
+            Assert.Contains( input,_csvDatabase.Read());
         }
         #endregion
     }
