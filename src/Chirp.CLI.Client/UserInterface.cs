8001297ef243fcb1de4bf86a18cd0ed7a01465a0
Joshua <jcal@itu.dk>
2023-09-21 21:51:22 +0200

    Now printing the output in the right format from web app
    Added also Newtonsoft.Json library to convert from string to JSON when reading from the web app

--- src/Chirp.CLI.Client/UserInterface.cs
+++ src/Chirp.CLI.Client/UserInterface.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 public static class UserInterface
 {
 
@@ -27,13 +31,17 @@ public static class UserInterface
             if (response.IsSuccessStatusCode)
             {
                 // Read the JSON content as a string
-                string jsonContent = await response.Content.ReadAsStringAsync();
+                var jsonContent = await response.Content.ReadAsStringAsync();
 
+                //Link to library use to convert a string to JSON object
+                //https://learn.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-using-the-dotnet-cli 
                 // Now you can parse the JSON content or work with it as needed
                 // For example, you can deserialize it into an object if you have a corresponding class
                 // using a JSON library like Newtonsoft.Json (Json.NET)
-                // For simplicity, let's just print the JSON content in this example
-                Console.WriteLine(jsonContent);
+                Cheep cheep = JsonConvert.DeserializeObject<Cheep>(jsonContent);
+                DateTime dateTime = UnixTimeParser(cheep.Timestamp);
+                
+                Console.WriteLine($"{cheep.Author} @ {dateTime}: {cheep.Message} ");
             }
             else
             {
