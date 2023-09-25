01b55eb0e4798e9bb2e4da328d4c237f9118ec91
Abdul Hanan <hanani.nas@gmail.com>
2023-09-25 17:42:16 +0200

    Fixed read
    Fixed read and added some code for adding new cheeps needs more work.

--- src/Chirp.CLI.Client/UserInterface.cs
+++ src/Chirp.CLI.Client/UserInterface.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Runtime.InteropServices.JavaScript;
+using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -13,7 +14,7 @@ public static class UserInterface
         return DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
     }
 
-    async public static Task printCheeps(IEnumerable<Cheep> cheeps)
+    async public static Task printCheeps(int? limit = null)
     {
         // UserInterface.printCheeps(database.Read());
         // URL of the JSON file
@@ -33,15 +34,18 @@ public static class UserInterface
                 // Read the JSON content as a string
                 var jsonContent = await response.Content.ReadAsStringAsync();
 
+                // Deserialize json object to list source https://stackoverflow.com/questions/34103498/how-do-i-deserialize-a-json-array-using-newtonsoft-json
+                List<Cheep> cheeps = JsonConvert.DeserializeObject<List<Cheep>>(jsonContent);
                 //Link to library use to convert a string to JSON object
                 //https://learn.microsoft.com/en-us/nuget/quickstart/install-and-use-a-package-using-the-dotnet-cli 
                 // Now you can parse the JSON content or work with it as needed
                 // For example, you can deserialize it into an object if you have a corresponding class
                 // using a JSON library like Newtonsoft.Json (Json.NET)
-                Cheep cheep = JsonConvert.DeserializeObject<Cheep>(jsonContent);
-                DateTime dateTime = UnixTimeParser(cheep.Timestamp);
-                
-                Console.WriteLine($"{cheep.Author} @ {dateTime}: {cheep.Message} ");
+                foreach (var cheep in cheeps)
+                {
+                    DateTime dateTime = UnixTimeParser(cheep.Timestamp);
+                    Console.WriteLine($"{cheep.Author} @ {dateTime}: {cheep.Message} ");
+                }
             }
             else
             {
@@ -53,5 +57,54 @@ public static class UserInterface
             Console.WriteLine($"HTTP request error: {ex.Message}");
         }
     }
+
+    async  public static Task appendFile(object[] args) {
+
+        // source https://stackoverflow.com/questions/68719641/send-json-data-in-http-post-request-c-sharp
+        // UserInterface.printCheeps(database.Read());
+        // URL of the JSON file
+        string jsonUrl = "https://bdsagroup17chirpremotedb.azurewebsites.net/cheep";
+
+ 
+
+        try
+        {
+        string message = "";
+        var user = Environment.UserName;
+        DateTime dateTime = DateTime.Now;
+        DateTimeOffset dto = new DateTimeOffset(dateTime.ToUniversalTime());
+        long unixDateTime = dto.ToUnixTimeSeconds();
+        for (int i = 0; i < args.Length; i++)
+        {
+            message += i == 0 ? args[i] : $" {args[i]}";
+        }
+        Cheep newCheep = new Cheep(user, message, unixDateTime);
+        
+        var jsonCheep = JsonConvert.SerializeObject(newCheep);
+        Console.WriteLine(jsonCheep);
+
+        var content = new StringContent(jsonCheep, Encoding.UTF8, "application/json");
+        
+        // Create an instance of HttpClient
+        using HttpClient client = new HttpClient();
+
+        HttpResponseMessage response = await client.PostAsync(jsonUrl, content);
+
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"Added {user} @ {dateTime}: \"{message}\"");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to add cheep. Status code: {response.StatusCode}");
+        }        
+        } catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"HTTP request error: {ex.Message}");
+        }
+    }
 }
+
 
