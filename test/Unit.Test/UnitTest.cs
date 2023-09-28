22b43cd185707eb2222a72540af73c6c3978886f
Burak123T <burak-9@live.dk>
2023-09-28 14:36:41 +0200

    Unit test removed
    Unit test removed for Unix time parsing

--- test/Unit.Test/UnitTest.cs
+++ test/Unit.Test/UnitTest.cs
@@ -6,12 +6,7 @@ public class UnitTest
     [Fact]
     public void Test1()
     {
-        // Arrange
-        var input = 1690978778;
-        // Act
-        var result = UserInterface.dateTime;
-        // Assert
-        Assert.Equal("02/08/2023 12:19:38", result.ToString());
+
     }
 }
 
