7dbb830f937fc7e719aa104a57c7ecef358662df
Joshua <jcal@itu.dk>
2023-09-28 15:57:27 +0200

    Fixed package kissing error

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -9,21 +9,17 @@ public interface ICheepService
 public class CheepService : ICheepService
 {
     // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
-        {
-            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
-            new CheepViewModel("Rasmus", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
-        };
 
     public List<CheepViewModel> GetCheeps()
     {
-        return _cheeps;
+        // DBFacade DB = new DBFacade();
+        return DBFacade.DatabaseQuery(@"SELECT * FROM message");
     }
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
         // filter by the provided author name
-        return _cheeps.Where(x => x.Author == author).ToList();
+        return null;
     }
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.Data.Sqlite;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
