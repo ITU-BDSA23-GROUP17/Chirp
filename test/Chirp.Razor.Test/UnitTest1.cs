222f9cc2e5c56960c1b140f3c7fb02b70759d2c3
Joshua <jcal@itu.dk>
2023-10-04 21:14:15 +0200

    Refactoring the test code

---
+++ test/Chirp.Razor.Test/UnitTest1.cs
@@ -0,0 +1,40 @@
+namespace Chirp.Razor.Test;
+
+using System.Collections;
+using System.Net;
+using System.Web;
+
+public class UnitTest1
+{
+    [Fact]
+    public async void TestHTTPRequest()
+    {
+        //Arrange
+        string jsonUrl = "https://bdsagroup17chirpremotedb.azurewebsites.net/cheeps";
+        using HttpClient client = new HttpClient();
+
+        //Act
+        HttpResponseMessage response = await client.GetAsync(jsonUrl);
+        var jsonContent = await response.Content.ReadAsStringAsync();
+
+        //Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotEmpty(jsonContent);
+    }
+
+    [Fact]
+    public async void ReadEndPointUserHelge()
+    {
+        //Arrange
+        string stringURL = "http://localhost:5273/Helge";
+        using HttpClient client = new HttpClient();
+
+        //Act
+        HttpResponseMessage response = await client.GetAsync(stringURL);
+        string documentContents = await response.Content.ReadAsStringAsync();
+
+        //Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotEmpty(documentContents);
+    }
+}
