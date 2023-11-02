6c627c90747390829853462fcf6275ad419995a1
Burak123T <burak-9@live.dk>
2023-11-02 19:52:54 +0100

    onpost works for now
    The OnPost method seems to work

--- src/Chirp.Web/Pages/Cheep.cshtml.cs
+++ src/Chirp.Web/Pages/Cheep.cshtml.cs
@@ -5,6 +5,13 @@ namespace Chirp.Web.Pages
 {
     public class CheepModel : PageModel
     {
+        [BindProperty]
+        public string GetNewCheepText { get; set; }
+
+        public void OnPost()
+        {
+            Response.Redirect("/");
+        }
         public void OnGet()
         {
             if (!User.Identity.IsAuthenticated)
