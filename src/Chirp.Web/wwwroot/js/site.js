256081a8c385467985e910a5826dedc28064f93b
Abdul Hanan <hanani.nas@gmail.com>
2023-11-09 09:13:07 +0100

    Add profile picture upload functionality
    Also a small update to tailwind

--- src/Chirp.Web/wwwroot/js/site.js
+++ src/Chirp.Web/wwwroot/js/site.js
@@ -23,3 +23,45 @@ function showWordsLeft() {
   charactersLeftElement.textContent =
     "Character left: " + (160 - textFromTextArea.length);
 }
+
+// TO UPLOAD profile pics
+const inputDiv = document.getElementById("input-div");
+const uploadButton = document.getElementById("upload-button");
+
+inputDiv.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  inputDiv.classList.add("border-blue-500");
+});
+
+inputDiv.addEventListener("dragleave", () => {
+  inputDiv.classList.remove("border-blue-500");
+});
+
+inputDiv.addEventListener("drop", (e) => {
+  e.preventDefault();
+  inputDiv.classList.remove("border-blue-500");
+
+  const files = e.dataTransfer.files;
+
+  console.log("Dropped files:", files);
+});
+
+inputDiv.addEventListener("dragenter", (e) => {
+  e.preventDefault();
+});
+
+const fileSelect = document.getElementById("fileSelect");
+const fileElem = document.getElementById("fileElem");
+
+fileSelect.addEventListener(
+  "click",
+  (e) => {
+    if (fileElem) {
+      fileElem.click();
+    }
+    const files = fileElem.files;
+    uploadButton.classlist.remove("hidden");
+    console.log("Selected files:", files);
+  },
+  false
+);
