ead6d328fe592a3d5e2eb10e2b03fed9d0fb5b6f
Joshua <jcal@itu.dk>
2023-11-09 14:17:14 +0100

    Removed Truncate table command

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -33,16 +33,6 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ChirpDBContext>();
-    //003fd7fc-7841-4cd0-abae-98f088a22b8b
-    var tableNames = context.Model.GetEntityTypes()
-        .Select(t => t.GetTableName())
-        .Distinct()
-        .ToList();
-
-    foreach (var tableName in tableNames)
-    {
-        context.Database.ExecuteSqlRaw($"TRUNCATE TABLE `{tableName}`;");
-    }
 
 }
 
