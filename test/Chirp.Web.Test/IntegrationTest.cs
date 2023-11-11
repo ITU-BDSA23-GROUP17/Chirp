1f0b91d737ec062e76be4c5679feaee21ba96056
Abdul Hanan <hanani.nas@gmail.com>
2023-11-11 16:53:50 +0100

    Add new packages and update unit tests for SQL
    And fixed integration test

--- test/Chirp.Web.Test/IntegrationTest.cs
+++ test/Chirp.Web.Test/IntegrationTest.cs
@@ -11,7 +11,7 @@ public class IntegrationTest
         {
                 //Remember to dotnet run before you run this test
                 //////// Arrange
-                string stringURL = "https://bdsagroup17chirprazor.azurewebsites.net/Helge";
+                string stringURL = "https://localhost:7102/Helge";
                 using HttpClient client = new HttpClient();
 
                 //////// Act
@@ -26,100 +26,19 @@ public class IntegrationTest
                 var htmlDocument = await parser.ParseDocumentAsync(documentContents);
 
                 //get name Helge from the html. Since the name is inside <strong> we can query select it and remove all spaces and \n to only get his name
-                var querySelectorElementStrong = htmlDocument.QuerySelector("strong");
+                var querySelectorElementStrong = htmlDocument.QuerySelector("strong").TextContent.Trim().Replace("\n", "");
                 if (querySelectorElementStrong == null)
                 {
                         throw new Exception("QuerySelector returned null!");
                 }
-                string htmlStrong = querySelectorElementStrong.TextContent;
-                string getName = htmlStrong.Replace(" ", "");
-                getName = getName.Replace("\n", "");
 
-                //Get the date, same principle as above
-                var querySelectorElementSmall = htmlDocument.QuerySelector("small");
+                //get message by Helge from the html. Since the name is inside <strong> we can query select it and remove all spaces and \n to only get his name
+                var querySelectorElementSmall = htmlDocument.QuerySelector("small").TextContent.Trim().Replace("\n", "");
                 if (querySelectorElementStrong == null)
                 {
                         throw new Exception("QuerySelector returned null!");
                 }
-                string htmlSmall = querySelectorElementStrong.TextContent;
-                string getDate = htmlSmall.Trim();
-                getDate = getDate.Replace("\n", "");
 
-                //Get the first message of Helge, same principle as above
-                //Will need to replace Helge and Date, since they both are also in the p tag
-                var querySelectorElementP = htmlDocument.QuerySelector("p");
-                if (querySelectorElementStrong == null)
-                {
-                        throw new Exception("QuerySelector returned null!");
-                }
-                string htmlP = querySelectorElementStrong.TextContent;
-                string getMessage = htmlP.Replace("\n", "");
-                getMessage = getMessage.Trim();
-                getMessage = getMessage.Replace(getName, "");
-                getMessage = getMessage.Replace(getDate, "");
-                getMessage = getMessage.Trim();
-
-                Assert.Equal("Helge", getName);
-                Assert.Equal("Hello, BDSA students!", getMessage);
-                Assert.Equal("— 08/01/2023 12:16:48", getDate);
-        }
-
-        [Fact]
-        public async void EndPointRasmusTest()
-        {
-                //Remember to dotnet run before you run this test
-                //////// Arrange
-                string stringURL = "https://bdsagroup17chirprazor.azurewebsites.net/Rasmus";
-                using HttpClient client = new HttpClient();
-
-                /////// Act
-                HttpResponseMessage response = await client.GetAsync(stringURL);
-                string documentContents = await response.Content.ReadAsStringAsync();
-
-                /////// Assert
-                Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
-
-                // Parse the HTML document
-                var parser = new HtmlParser();
-                var htmlDocument = await parser.ParseDocumentAsync(documentContents);
-
-                //get name Helge from the html. Since the name is inside <strong> we can query select it and remove all spaces and \n to only get his name
-                var querySelectorElementStrong = htmlDocument.QuerySelector("strong");
-                if (querySelectorElementStrong == null)
-                {
-                        throw new Exception("QuerySelector returned null!");
-                }
-                string htmlStrong = querySelectorElementStrong.TextContent;
-                string getName = htmlStrong.Replace(" ", "");
-                getName = getName.Replace("\n", "");
-
-                //Get the date, same principle as above
-                var querySelectorElementSmall = htmlDocument.QuerySelector("small");
-                if (querySelectorElementSmall == null)
-                {
-                        throw new Exception("QuerySelector returned null!");
-                }
-                string htmlSmall = querySelectorElementSmall.TextContent;
-                string getDate = htmlSmall.Trim();
-                getDate = getDate.Replace("\n", "");
-
-                //Get the first message of Helge, same principle as above
-                //Will need to replace Helge and Date, since they both are also in the p tag
-                var querySelectorElementP = htmlDocument.QuerySelector("p");
-                if (querySelectorElementP == null)
-                {
-                        throw new Exception("QuerySelector returned null!");
-                }
-                string htmlP = querySelectorElementP.TextContent;
-                string getMessage = htmlP.Replace("\n", "");
-                getMessage = getMessage.Trim();
-                getMessage = getMessage.Replace(getName, "");
-                getMessage = getMessage.Replace(getDate, "");
-                getMessage = getMessage.Trim();
-
-                Assert.Equal("Rasmus", getName);
-                Assert.Equal("Hej, velkommen til kurset.", getMessage);
-                Assert.Equal("— 08/01/2023 13:08:28", getDate);
-
+                Assert.Equal("Hello, BDSA students!", querySelectorElementSmall);
         }
 }
