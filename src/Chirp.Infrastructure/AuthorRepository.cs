a9508a51bb33d93c8f72b7fa5caa5820a4373b8c
Burak123T <buoe@itu.dk>
2023-11-30 14:39:02 +0100

    Status works
    Status now reads and shows properly
    
    Co-authored-by: Hanan
    <hanan.nas@gmail.com>

--- src/Chirp.Infrastructure/AuthorRepository.cs
+++ src/Chirp.Infrastructure/AuthorRepository.cs
@@ -18,7 +18,7 @@ namespace Chirp.Infrastructure
         public async Task InsertAuthorAsync(string Name, string Email)
         {
             Guid guid = Guid.NewGuid();
-            await context.Authors.AddAsync(new Author() { AuthorId = guid.ToString(), Name = Name, Email = Email, Status = "OFFLINE" });
+            await context.Authors.AddAsync(new Author() { AuthorId = guid.ToString(), Name = Name, Email = Email, Status = "ONLINE" });
         }
 
         public async Task SaveAsync()
@@ -31,7 +31,7 @@ namespace Chirp.Infrastructure
             var Author = await context.Authors.Where(a => a.Email == Email).FirstOrDefaultAsync();
             if (Author != null)
             {
-                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, "OFFLINE", Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
+                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, Author.Status, Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
             }
             else
             {
@@ -44,7 +44,7 @@ namespace Chirp.Infrastructure
             var Author = await context.Authors.FindAsync(AuthorId);
             if (Author != null)
             {
-                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, "OFFLINE", Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
+                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, Author.Status, Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
             }
             else
             {
@@ -57,7 +57,7 @@ namespace Chirp.Infrastructure
             var Author = await context.Authors.Where(a => a.Name == Name).FirstOrDefaultAsync();
             if (Author != null)
             {
-                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, "OFFLINE", Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
+                return new AuthorDTO(Author.AuthorId, Author.Name, Author.Email, Author.Status, Author.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList());
             }
             else
             {
@@ -116,7 +116,7 @@ namespace Chirp.Infrastructure
                  .Contains(a.AuthorId))
                  .ToListAsync();
 
-            var authorDTOs = authors.Select(a => new AuthorDTO(a.AuthorId, a.Name, a.Email, "OFFLINE", a.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList())).ToList();
+            var authorDTOs = authors.Select(a => new AuthorDTO(a.AuthorId, a.Name, a.Email, a.Status, a.Cheeps.Select(c => new CheepDTO(c.CheepId, c.Text, c.TimeStamp, c.Author.Name, c.Author.AuthorId)).ToList())).ToList();
             return authorDTOs;
         }
 
@@ -130,5 +130,6 @@ namespace Chirp.Infrastructure
                 context.Authors.Update(authorToUpdate);
             }
         }
+
     }
 }
