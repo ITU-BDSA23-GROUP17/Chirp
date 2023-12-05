4338a505e86804b03cbb49447386f61726fe381b
Ch Hanan Hadayat <hanani.nas@gmail.com>
2023-12-05 10:32:40 +0100

    Add PlaywrightTests.cs and remove TitleShouldContainsPlaywright.cs

---
+++ test/Chirp.Web.Test/Playwright.Test/PlaywrightTests/PlaywrightTests.cs
@@ -0,0 +1,39 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class PlaywrightTests : PageTest
+{
+    [Test]
+    public async Task CheckElementPresence()
+    {
+        await Page.GotoAsync("https://bdsagroup17chirprazor.azurewebsites.net/");
+
+
+        // Expect a title to have title Chirp!.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
+
+        // Check that the "cheep" element is present
+        var cheepElement = await Page.QuerySelectorAsync(".text-blue-400");
+        Assert.IsNotNull(cheepElement);
+
+        // Get the text of the "cheep" element
+        string cheepText = await cheepElement.InnerTextAsync();
+        Assert.That(cheepText, Is.EqualTo("Hello josh"));
+
+        // i want to see if the page has a url is present 
+        var check = await Page.GetByRole(AriaRole.Link, new() { Name = "hananinas" }).IsVisibleAsync();
+
+        Assert.True(check);
+
+        // check if the link is clickable
+        await Page.GetByRole(AriaRole.Link, new() { Name = "hananinas" }).ClickAsync();
+
+    }
+}
