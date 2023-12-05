05ce1d685913df13ce9afa46b3e4bf21fc7a338c
Ch Hanan Hadayat <hanani.nas@gmail.com>
2023-12-05 10:07:13 +0100

    Created first test with Playwright

--- test/Chirp.Web.Test/Playwright.Test/PlaywrightTests/TitleShouldContainsPlaywright.cs
+++ test/Chirp.Web.Test/Playwright.Test/PlaywrightTests/TitleShouldContainsPlaywright.cs
@@ -11,23 +11,14 @@ namespace PlaywrightTests;
 public class PlaywrightTests : PageTest
 {
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task CheckElementPresence()
     {
-        await Page.GotoAsync("https://playwright.dev");
+        await Page.GotoAsync("https://bdsagroup17chirprazor.azurewebsites.net/");
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        // Check that the "cheep" element is present
+        var cheepElement = await Page.QuerySelectorAsync(".text-blue-400");
+        Assert.IsNotNull(cheepElement);
     }
+
+
 }
