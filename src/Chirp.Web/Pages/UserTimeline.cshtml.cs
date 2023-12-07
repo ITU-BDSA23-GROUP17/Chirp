39b5bcf5541e800388f3ba08c0bd238f503a7ea8
Joshua <jcal@itu.dk>
2023-12-07 12:11:07 +0100

    Change from int to string in getTotalReaction

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -171,17 +171,17 @@ IReactionRepository reactionRepository)
         return HttpContext.GetRouteValue("author").ToString();
     }
 
-    public int getTotalReactions(string cheepId)
+    public string getTotalReactions(string cheepId)
     {
 
         var total = _reactionRepository.GetTotalReactionsByCheepId(cheepId).ToString();
         if (total == null)
         {
-            return 0;
+            return "0";
         }
         else
         {
-            return Int32.Parse(total);
+            return total;
         }
     }
 
