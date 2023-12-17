9218cbe3a81257ba44d424133eb996db917ee4aa
Ch Hanan Hadayat <hanani.nas@gmail.com>
2023-12-17 14:14:34 +0100

    Update connection string for production environment

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -38,8 +38,8 @@ builder.Services.AddScoped<IHashtagRepository, HashtagRepository>();
 
 if (builder.Environment.IsDevelopment())
 {
-    KeyVaultSecret secret = client.GetSecret("burak-connectionstring");
-    connectionString = secret.Value;
+    KeyVaultSecret secret = client.GetSecret("prod-connectionstring");
+    connectionString = "Server=tcp:chirp17pro.database.windows.net,1433;Initial Catalog=Hanan-chirp;Persist Security Info=False;User ID=chirpadmin;Password=Nasd!234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 }
 else
 {
