ff9799c4be062a807b8873d20412ab85b8c3864d
Ch Hanan Hadayat <hanani.nas@gmail.com>
2023-12-19 10:43:30 +0100

    Refactor HashtagModel and _TimeLinePartial

--- src/Chirp.Web/Pages/Hashtag.cshtml.cs
+++ src/Chirp.Web/Pages/Hashtag.cshtml.cs
@@ -16,12 +16,14 @@ using System.Drawing.Printing;
 
 namespace Chirp.Web.Pages;
 
-public class HashtagModel : PageModel
+public class HashtagModel : BaseModel
 {
     //(We initialize with standard placeholder values to be overwritten later, to avoid
     //'Non-nullable property must contain a non-null value when exiting constructor.' warning'))
     public IEnumerable<CheepDTO> Cheeps { get; set; } = new List<CheepDTO>();
     public IEnumerable<CheepInfoDTO> CheepInfos { get; set; } = new List<CheepInfoDTO>();
+    List<CheepInfoDTO> CheepInfoList = new List<CheepInfoDTO>();
+
     public int pageNr { get; set; } = 0;
     public int pages { get; set; } = 0;
 
@@ -32,24 +34,12 @@ public class HashtagModel : PageModel
     public List<String> uniqueHashtagTexts { get; set; } = null;
     public List<String> popularHashtags { get; set; } = null;
 
-    [BindProperty]
-    public IFormFile Upload { get; set; }
-
-
-    private readonly ICheepRepository _cheepRepository;
-    private readonly IAuthorRepository _authorRepository;
-    private readonly IFollowRepository _followRepository;
-    private readonly IReactionRepository _reactionRepository;
     private readonly IHashtagRepository _hashtagRepository;
     private readonly IHashtagTextRepository _hashtagTextRepository;
 
     public HashtagModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository, IFollowRepository followRepository,
-    IReactionRepository reactionRepository, IHashtagRepository hashtagRepository, IHashtagTextRepository hashtagTextRepository)
+    IReactionRepository reactionRepository, IHashtagRepository hashtagRepository, IHashtagTextRepository hashtagTextRepository) : base(cheepRepository, authorRepository, followRepository, reactionRepository)
     {
-        _cheepRepository = cheepRepository;
-        _authorRepository = authorRepository;
-        _followRepository = followRepository;
-        _reactionRepository = reactionRepository;
         _hashtagRepository = hashtagRepository;
         _hashtagTextRepository = hashtagTextRepository;
     }
@@ -91,22 +81,26 @@ public class HashtagModel : PageModel
         // pages = _service.getPagesHome(true, author);
         pages = _cheepRepository.getPagesFromCheepCount(Cheeps.Count());
 
-        //We need to do some work to get the CheepInfo. First find Cheeps, then make CheepInfoDTOs.
-        //We need the following ids in the else statement and below therefore it's here....
-        List<string> followingIDs = await _followRepository.GetFollowingIDsByAuthorIDAsync(currentlyLoggedInUser.AuthorId);
-        List<string> reactionCheepIds = await _reactionRepository.GetCheepIdsByAuthorId(currentlyLoggedInUser.AuthorId);
 
-        List<CheepInfoDTO> CheepInfoList = new List<CheepInfoDTO>();
-        //To get the CheepInfos we need to do some work...
-        foreach (CheepDTO cheep in Cheeps)
+
+        if (currentlyLoggedInUser != null)
         {
-            CheepInfoDTO cheepInfoDTO = new CheepInfoDTO
+            //We need to do some work to get the CheepInfo. First find Cheeps, then make CheepInfoDTOs.
+            //We need the following ids in the else statement and below therefore it's here....
+            List<string> followingIDs = await _followRepository.GetFollowingIDsByAuthorIDAsync(currentlyLoggedInUser.AuthorId);
+            List<string> reactionCheepIds = await _reactionRepository.GetCheepIdsByAuthorId(currentlyLoggedInUser.AuthorId);
+
+            //To get the CheepInfos we need to do some work...
+            foreach (CheepDTO cheep in Cheeps)
             {
-                Cheep = cheep,
-                UserIsFollowingAuthor = IsUserFollowingAuthor(cheep.AuthorId, followingIDs),
-                UserReactToCheep = IsUserReactionCheep(cheep.Id, reactionCheepIds)
-            };
-            CheepInfoList.Add(cheepInfoDTO);
+                CheepInfoDTO cheepInfoDTO = new CheepInfoDTO
+                {
+                    Cheep = cheep,
+                    UserIsFollowingAuthor = IsUserFollowingAuthor(cheep.AuthorId, followingIDs),
+                    UserReactToCheep = IsUserReactionCheep(cheep.Id, reactionCheepIds)
+                };
+                CheepInfoList.Add(cheepInfoDTO);
+            }
         }
 
 
@@ -122,99 +116,5 @@ public class HashtagModel : PageModel
 
         return Page();
     }
-
-    public bool IsUserFollowingAuthor(string authorID, List<string> followingIDs)
-    {
-        {
-            return followingIDs.Contains(authorID);
-        }
-    }
-
-    public bool IsUserReactionCheep(string cheepId, List<string> reactionAuthorId)
-    {
-        {
-            return reactionAuthorId.Contains(cheepId);
-        }
-    }
-
-
-    public async Task<IActionResult> OnPostFollow(string authorName, string follow, string? unfollow)
-    {
-        var Claims = User.Claims;
-        var email = Claims.FirstOrDefault(c => c.Type == "emails")?.Value;
-        currentlyLoggedInUser = await _authorRepository.GetAuthorByEmailAsync(email);
-        var isUserFollowingAuthor = await _authorRepository.GetAuthorByIdAsync(authorName);
-
-        Console.WriteLine(currentlyLoggedInUser);
-
-        if (follow != null)
-        {
-            await _followRepository.InsertNewFollowAsync(currentlyLoggedInUser.AuthorId, authorName);
-        }
-        if (unfollow != null)
-        {
-            await _followRepository.RemoveFollowAsync(currentlyLoggedInUser.AuthorId, authorName);
-        }
-
-
-        return Redirect("/" + isUserFollowingAuthor.Name.Replace(" ", "%20"));
-    }
-
-    public async Task<IActionResult> OnPostReaction(string cheepId, string authorId, string reaction)
-    {
-        var Claims = User.Claims;
-        var email = Claims.FirstOrDefault(c => c.Type == "emails")?.Value;
-
-        currentlyLoggedInUser = await _authorRepository.GetAuthorByEmailAsync(email);
-
-        var likeID = "fbd9ecd2-283b-48d2-b82a-544b232d6244";
-
-        if (currentlyLoggedInUser == null)
-        {
-            Console.WriteLine("Can not react to cheep, user is not logged in");
-        }
-        bool hasReacted = await _reactionRepository.CheckIfAuthorReactedToCheep(cheepId, currentlyLoggedInUser.AuthorId);
-        if (hasReacted)
-        {
-            Console.WriteLine("Removed like on " + cheepId);
-            await _reactionRepository.RemoveReactionAsync(cheepId, currentlyLoggedInUser.AuthorId);
-        }
-        else
-        {
-            Console.WriteLine("Added like on " + cheepId);
-            await _reactionRepository.InsertNewReactionAsync(cheepId, currentlyLoggedInUser.AuthorId);
-        }
-
-        Console.WriteLine(HttpContext.Request.Path);
-
-        //When using RedirectToPage() in / root and in public timline it will redirect to /Public, and /public is not a valid page. 
-        if (HttpContext.Request.Path == "/Public")
-        {
-            return Redirect("/");
-        }
-        else
-        {
-            return RedirectToPage();
-        }
-
-    }
-    public async Task<string> getStatus()
-    {
-        string? viewedUser = HttpContext?.GetRouteValue("author")?.ToString();
-        var StatusAuthorDTO = await _authorRepository.GetAuthorByNameAsync(viewedUser);
-        var Status = StatusAuthorDTO?.Status;
-        Console.WriteLine("Received user: " + viewedUser);
-        Console.WriteLine("Received status: " + Status);
-        return Status;
-    }
-    public async Task<string> getStatusPublic(string name)
-    {
-        var StatusAuthorDTO = await _authorRepository.GetAuthorByNameAsync(name);
-        var Status = StatusAuthorDTO?.Status;
-        Console.WriteLine("Received user: " + name);
-        Console.WriteLine("Received status: " + Status);
-        return Status;
-    }
-
 
 }
