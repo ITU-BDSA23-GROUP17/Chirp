4dbb27974a0064b6f4f7f74f8eeaa518359555a6
Tan Dang <tandang2002@live.dk>
2023-12-19 16:26:55 +0100

    Update AuthorRepositoryTest.cs

--- test/Chirp.Infrastructure.Test/AuthorRepositoryTest.cs
+++ test/Chirp.Infrastructure.Test/AuthorRepositoryTest.cs
@@ -47,51 +47,7 @@ public sealed class AuthorRepositoryUnitTest : IAsyncLifetime
     }
 
     [Fact]
-    public async Task InsertCheepAddsCheepToDatabase()
-    {
-        // Arrange
-        // Start the container
-        await _msSqlContainer.StartAsync();
-
-        // Arrange
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlServer(_connectionString);
-        using var context = new ChirpDBContext(builder.Options);
-        context.initializeDB(); //ensure all tables are created
-
-        //Cheep and author repository created
-        ICheepRepository cheepRepository = new CheepRepository(context);
-        IAuthorRepository authorRepository = new AuthorRepository(context);
-
-        //Getting authorDTO by the name Helge
-        var authorDTOTest = await authorRepository.GetAuthorByNameAsync("Helge");
-        if (authorDTOTest == null)
-        {
-            throw new Exception("Could not find author Helge");
-        }
-        //Getting the authorID from AuthorDTO
-        var authorId = authorDTOTest.AuthorId;
-
-        //We create a cheep
-        var cheepDto = new CheepDTO(
-            Id: "asdasd",
-            Message: "test message cheep",
-            TimeStamp: DateTime.Now,
-            AuthorName: "Helge",
-            AuthorId: authorId,
-            AuthorImage: ""
-            );
-
-        // Act
-        cheepRepository.InsertCheep(cheepDto);
-        context.SaveChanges(); // Save changes to in-memory database
-
-        // Assert
-        var insertedCheep = context.Cheeps.FirstOrDefault(c => c.Text == "test message cheep");
-        Assert.NotNull(insertedCheep); // Check that we get a cheep
-        Assert.Equal("test message cheep", insertedCheep.Text);
-    }
-    [Fact]
-    public async Task GetAuthorByEmailAsync()
+    public async Task GetAuthorByEmailAsync_ReturnsCorrectAuthor()
     {
         // Start the container
         await _msSqlContainer.StartAsync();
@@ -117,7 +73,7 @@ public sealed class AuthorRepositoryUnitTest : IAsyncLifetime
         await _msSqlContainer.StopAsync();
     }
     [Fact]
-    public async Task GetAuthorByNameAsync()
+    public async Task GetAuthorByNameAsync_ReturnsCorrectAuthor()
     {
         // Start the container
         await _msSqlContainer.StartAsync();
